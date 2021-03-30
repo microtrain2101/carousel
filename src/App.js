665cd06ba42abf5e84d4aefb789357c89fe36d10
chad svastisalee <swasgrafx@gmail.com>
2021-03-30 10:20:08 -0500

    Final Build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+let styles = {
+    margin: 'auto',
+    // height: '480px',
+    width: '500px'
+  };
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div style={styles}>
+<Carousel>
+<div>
+<img src="/images/aquaman.jpg" alt="Aquaman" />
+<p className="legend">Aquaman</p>
+</div>
+<div>
+<img src="/images/batman.jpg" alt="Batman"/>
+<p className="legend">Batman</p>
+</div>
+<div>
+<img src="/images/cyborg.jpg" alt="Cyborg"/>
+<p className="legend">Cyborg</p>
+</div>
+<div>
+<img src="/images/deathstroke.jpg" alt="Deathstroke"/>
+<p className="legend">Deathstroke</p>
+</div>
+<div>
+<img src="/images/flash.jpg" alt="Flash"/>
+<p className="legend">Flash</p>
+</div>
+<div>
+<img src="/images/superman.jpg" alt="Superman"/>
+<p className="legend">Superman</p>
+</div>
+<div>
+<img src="/images/wonder_woman.jpg" alt="Wonder Woman"/>
+<p className="legend">Wonder Woman</p>
+</div>
+</Carousel>
+</div>
   );
 }
 
